668f6c59b80924487410c585d3c631a1f7600209
abhishekvarmagh <abhishektheverma@gmail.com>
2020-02-17 18:48:48 +0530

    ADDED : Welcome Message

---
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.sql.SQLOutput;
+
+public class UserValidator {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Junit User Registration");
+    }
+}
